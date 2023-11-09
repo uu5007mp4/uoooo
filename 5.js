c12d46b26496376bf0d91b909503832a4ec39918
uu5007mp4 <86598757+uu5007mp4@users.noreply.github.com>
2023-11-09 21:47:21 +0900

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+
+document.getElementById('webhook-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const webhookURL = document.getElementById('webhook-url').value;
+    sendMessage(webhookURL);
+});
+
+function sendMessage(webhookURL) {
+    const username = document.getElementById('username').value;
+    let message = document.getElementById('message').value;
+    const avatarUrl = document.getElementById('avatar-url').value;
+    const messageFile = document.getElementById('message-file-upload').files[0];
+
+    message = message.replace(/@/g, '');
+
+    const payload = new FormData();
+    payload.append('username', username);
+    payload.append('content', message);
+
+    if (avatarUrl) {
+        payload.append('avatar_url', avatarUrl);
+    }
+
+    if (messageFile) {
+        payload.append('file', messageFile);
+    }
+
+    fetch(webhookURL, {
+        method: 'POST',
+        body: payload,
+    })
+}, 10); // 10ミリ秒ごとに実行（0.01秒）
